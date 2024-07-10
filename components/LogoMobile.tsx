b9d8ff7da2768deebd79cfca2992f63b64eec1c1
Gary Wallen <garycwallen@gmail.com>
2024-07-09 22:44:57 -0400

    Setup Desktop and Mobile Navbar

--- components/LogoMobile.tsx
+++ components/LogoMobile.tsx
@@ -1,10 +1,9 @@
 import { PiggyBank } from 'lucide-react';
 import React from 'react';
 
-export default function Logo() {
+export default function LogoMobile() {
   return (
     <a href='/' className='flex item-center gap-2'>
-      <PiggyBank className='stroke h-11 w-11 stroke-amber-500 stroke-[1.5]' />
       <p className='bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent'>
         FamilyLedger
       </p>
